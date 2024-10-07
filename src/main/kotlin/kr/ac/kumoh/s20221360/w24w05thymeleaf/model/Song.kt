5977ef6b82fbc171f4234b5fae928bac62092216
RRRdj <emforhs3213@naver.com>
2024-10-07 16:24:01 +0900

    🍱 random 노래 제목

---
+++ src/main/kotlin/kr/ac/kumoh/s20221360/w24w05thymeleaf/model/Song.kt
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.s20221360.w24w05thymeleaf.model
+
+data class Song(
+    var id: Int,
+    var title: String,
+    var singer: String,
+)
---
+++ src/main/kotlin/kr/ac/kumoh/s20221360/w24w05thymeleaf/service/SongService.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20221360.w24w05thymeleaf.service
+
+import kr.ac.kumoh.s20221360.w24w05thymeleaf.repository.SongRepository
+import org.springframework.stereotype.Service
+
+
+@Service
+class SongService(val repository: SongRepository) {
+    fun getRandomSong() = repository.getSong(Random.nextInt(repository.songsSize))
+    fun getAllSongs() = repository.fetchSong()
+}
