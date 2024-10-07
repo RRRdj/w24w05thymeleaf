bc14db990252c429e42aae55514e545278ec89a6
RRRdj <emforhs3213@naver.com>
2024-10-07 16:15:33 +0900

    💡 model 추가

--- src/main/kotlin/kr/ac/kumoh/s20221360/w24w05thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20221360/w24w05thymeleaf/controller/SongController.kt
@@ -1,13 +1,15 @@
 package kr.ac.kumoh.s20221360.w24w05thymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 
 @Controller
 class SongController {
     @GetMapping("/song/random")
-    fun getRandomSong() : String {
+    fun getRandomSong(model: Model) : String {
+        model.addAttribute("songTitle","소주 한 잔")
         return "random"
     }
 }
